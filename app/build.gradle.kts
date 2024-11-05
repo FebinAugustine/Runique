38c9813f90e5d601eeb31692766dfaa0b78d9252
Febin <febinaugustine7@gmail.com>
2024-11-05 23:29:57 +0530

    Convention Plugins: Android Library Convention Plugin Added

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,9 +1,10 @@
 plugins {
 //    alias(libs.plugins.android.application)
 //    alias(libs.plugins.kotlin.android)
-    alias(libs.plugins.kotlin.compose)
+//    alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.runique.android.application.compose)
     alias(libs.plugins.mapsplatform.secrets.plugin)
-    alias(libs.plugins.runique.android.application)
+
 }
 
 android {
@@ -23,12 +24,12 @@ android {
 //    kotlinOptions {
 //        jvmTarget = "11"
 //    }
-    buildFeatures {
-        compose = true
-    }
-    composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.1"
-    }
+//    buildFeatures {
+//        compose = true
+//    }
+//    composeOptions {
+//        kotlinCompilerExtensionVersion = "1.5.1"
+//    }
     packaging {
         resources {
             excludes += "/META-INF/{AL2.0,LGPL2.1}"
