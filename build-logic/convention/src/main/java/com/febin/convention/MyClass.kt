37816c3dfbf0974139745d4da8779e36bce00716
Febin <febinaugustine7@gmail.com>
2024-11-05 18:21:04 +0530

    Gradle Management First Stage Complete

---
+++ build-logic/convention/src/main/java/com/febin/convention/MyClass.kt
@@ -0,0 +1,4 @@
+package com.febin.convention
+
+class MyClass {
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,4 +1,5 @@
 pluginManagement {
+    includeBuild("build-logic")
     repositories {
         google {
             content {
@@ -20,6 +21,7 @@ dependencyResolutionManagement {
 }
 
 rootProject.name = "Runique"
+enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
 include(":app")
 include(":auth:data")
 include(":auth:domain")
@@ -34,3 +36,4 @@ include(":run:domain")
 include(":run:presentation")
 include(":run:location")
 include(":run:network")
+
