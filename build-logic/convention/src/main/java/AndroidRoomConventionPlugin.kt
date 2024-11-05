ab58c0464efd005e898da94d2d32e9005b4279e2
Febin <febinaugustine7@gmail.com>
2024-11-06 00:10:33 +0530

    Convention Plugins: Room Convention Plugin Added

---
+++ build-logic/convention/src/main/java/AndroidRoomConventionPlugin.kt
@@ -0,0 +1,35 @@
+import androidx.room.gradle.RoomExtension
+import com.android.build.api.dsl.LibraryExtension
+import com.febin.convention.ExtensionType
+import com.febin.convention.configureAndroidCompose
+import com.febin.convention.configureBuildTypes
+import com.febin.convention.configureKotlinAndroid
+import com.febin.convention.libs
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+import org.gradle.kotlin.dsl.configure
+import org.gradle.kotlin.dsl.dependencies
+import org.gradle.kotlin.dsl.getByType
+import org.gradle.kotlin.dsl.kotlin
+
+class AndroidRoomConventionPlugin: Plugin<Project> {
+
+    override fun apply(target: Project) {
+        target.run {
+            pluginManager.run {
+                apply("androidx.room")
+                apply("com.google.devtools.ksp")
+            }
+
+            extensions.configure<RoomExtension> {
+                schemaDirectory("$projectDir/schemas")
+            }
+
+            dependencies {
+                "implementation"(libs.findLibrary("room.runtime").get())
+                "implementation"(libs.findLibrary("room.ktx").get())
+                "ksp"(libs.findLibrary("room.compiler").get())
+            }
+        }
+    }
+}
